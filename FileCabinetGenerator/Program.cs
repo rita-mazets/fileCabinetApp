63bbee99a46a189fb43062e24aece0bf72c3aace
rita-mazets <mmmazets@mail.ru>
2021-12-21 06:14:00 +0300

    Add output-type output records-amount start-id parameter for FileCabinetGenerator

--- FileCabinetGenerator/Program.cs
+++ FileCabinetGenerator/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace FileCabinetGenerator
 {
@@ -7,6 +8,55 @@ namespace FileCabinetGenerator
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string outputType, output, recordAmounr, startId;
+            (outputType, output, recordAmounr, startId) = ParseArgs(args);
+        }
+
+        private static (string, string, string, string) ParseArgs(string[] args)
+        {
+            string nameOutputTypeParam = "csv";
+            string nameOutputParam = "D:\\records.csv";
+            string nameRecordAmountParam = "100";
+            string nameStartIdParam = "1";
+
+            if (args is not null)
+            {
+                for (int i = 0; i < args.Length; i++)
+                {
+                    nameOutputTypeParam = CheckParam(args, i, "--output-type=", "-t", nameOutputTypeParam);
+                    nameOutputParam = CheckParam(args, i, "--output=", "-o", nameOutputParam);
+                    nameRecordAmountParam = CheckParam(args, i, "--records-amount=", "-a", nameRecordAmountParam);
+                    nameStartIdParam = CheckParam(args, i, "--start-id=", "-i", nameStartIdParam);
+                }
+            }
+
+            return (nameOutputTypeParam, nameOutputParam, nameRecordAmountParam, nameStartIdParam);
+        }
+
+        private static string CheckParam(string[] args, int i, string fullNameParam, string shortNameParam, string valueParam)
+        {
+            bool isShort = false;
+
+            if (args[i].ToLower(CultureInfo.CurrentCulture).Contains(fullNameParam))
+            {
+                valueParam = args[i].Split("=")[1];
+            }
+
+            if (args[i].ToLower(CultureInfo.CurrentCulture).Equals(shortNameParam))
+            {
+                isShort = true;
+            }
+
+            if (isShort)
+            {
+                if (i + 1 < args.Length)
+                {
+                    valueParam = args[i + 1];
+                    isShort = false;
+                }
+            }
+
+            return valueParam;
         }
     }
 }
